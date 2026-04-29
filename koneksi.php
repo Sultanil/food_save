9f301e7fd549678f3f0c997961959827c0842078
Fyqq <syafiqsyadi07@student.uns.ac.id>
2026-04-30 00:59:27 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "food_save",);
+$conn = mysqli_connect("localhost", "root", "root", "food_save",);
 
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
